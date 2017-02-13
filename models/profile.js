da74e392425f1bcc37f8599b1585c57824888603
chri-roch12 <chris.prochnow@gmail.com>
2017-02-12 22:43:41 -0800

    Found out tht I could hard-code diretly into server.js for this info, removed dependency from index.

--- models/profile.js
+++ models/profile.js
@@ -1,15 +1,15 @@
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
-
-var ProfileSchema = new Schema({
-  name: String,
-  githubLink: "https://github.com/chri-roch12/express-personal-api",
-  githubProfileImage: "https://avatars2.githubusercontent.com/u/25183085?v=3&s=400",
-  personalSiteLink: "https://chri-roch12.github.io",
-  currentCity: "Berkeley, CA",
-  Siblings: [{name: "Jon Prochnow", type: "brother", age: "32"}],
-});
-
-var Beer = mongoose.model("Beer", BeerRecipeSchema);
-
-module.exports = Beer;
+// var mongoose = require('mongoose'),
+//   Schema = mongoose.Schema;
+//
+// var ProfileSchema = new Schema({
+//   name: "Chris Prochnow",
+//   githubLink: "https://github.com/chri-roch12/express-personal-api",
+//   githubProfileImage: "https://avatars2.githubusercontent.com/u/25183085?v=3&s=400",
+//   personalSiteLink: "https://chri-roch12.github.io",
+//   currentCity: "Berkeley, CA",
+//   Siblings: {name: "Jon Prochnow", type: "brother", age: "32"},
+// });
+//
+// var Beer = mongoose.model("profile", ProfileSchema);
+//
+// module.exports = Profile;
