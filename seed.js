f3e81b50dca6cf0456fa0b048705ed8b46f96eb7
chri-roch12 <chris.prochnow@gmail.com>
2017-02-12 22:47:06 -0800

    Honestly don't know what I changed here. I did not need to make any changes, so maybe I added a blank space somewhere.

--- seed.js
+++ seed.js
@@ -1,19 +1,3 @@
-// This file allows us to seed our application with data
-// simply run: `node seed.js` from the root of this project folder.
-
-// var db = require('./models');
-
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
-
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
-
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
-
 var db = require('./models');
 
 var beer_list = [
@@ -49,5 +33,5 @@ db.Beer.create(beer_list, function(err, beer){
   }
 
   console.log("Created new beer list", beer._id)
-  process.exit(); // we're all done! Exit the program.
+  process.exit(); 
 })
